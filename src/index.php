242fe83e8e74c67327858c76b0cdd38ef345735d
Imre Boersma <imre.boersma@gmail.com>
2019-11-26 14:21:04 +0000

    Merged in breadcrumbs (pull request #21)
    Breadcrumbs
    
    * routing
    
    * htacces
    
    * first model with parent
    
    * ModelHelper, Connect en cofig file gefixt
    
    * voorbeeld
    
    * commit
    
    * templates
    
    * commit
    
    * commit
    
    * send mail with secret code and add safety question
    
    * commit
    
    * added breadcrumbs to index.php
    
    Approved-by: B.vanKleef <b.vankleef@student.han.nl>
    Approved-by: Bram Klapwijk <bram.klapwijk00@gmail.com>
    Approved-by: Timo Vaassen <timovaassen1@kpnmail.nl>

--- src/index.php
+++ src/index.php
@@ -1,28 +1,32 @@
 <?php
-
-    require_once('config.php');
-    
+require_once('config.php');
 ?>
 
 <!doctype html>
 <html lang="nl">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Eenmaal Andermaal</title>
-        <link rel="stylesheet" href="<?php echo Config::ROOT_FOLDER ."/css/bootstrap.css" ?>">
-    </head>
-    <body>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Eenmaal Andermaal</title>
+    <link rel="stylesheet" href="<?php echo Config::ROOT_FOLDER . "/css/bootstrap.css" ?>">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body>
     <header>
         <?php require('views/shared/header.php'); ?>
     </header>
 
-    <main role="main" style="height:100%!important">
-        <div class="album py-5 bg-light">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12 col-sm-12 col-lg-12">
+    <main role="main">
+        <div class="py-4 bg-light">
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <?php
+                        require('views/shared/breadcrumbs.php');
+                        ?>
                         <?php require 'routes.php'; ?>
                     </div>
                 </div>
@@ -30,14 +34,14 @@
         </div>
     </main>
 
-    <footer class="footer mt-auto py-3">
-        <div class="container">
+    <footer class="py-3 bg-secondary">
+        <div class="container-fluid">
             <?php require('views/shared/footer.php'); ?>
         </div>
     </footer>
+    <script src="<?php echo Config::ROOT_FOLDER . "/js/bootstrap.js" ?>" type="text/javascript"></script>
+    <script src="<?php echo Config::ROOT_FOLDER . "/js/jquery.min.js" ?>" type="text/javascript"></script>
 
-    <script src="<?php echo Config::ROOT_FOLDER ."/js/bootstrap.js" ?>" type="text/javascript"></script>
-    <script src="<?php echo Config::ROOT_FOLDER ."/js/jquery.min.js" ?>" type="text/javascript"></script>
 </body>
-</html>
 
+</html>
---
+++ src/views/shared/breadcrumbs.php
@@ -0,0 +1,14 @@
+<div aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item active" aria-current="page"><a href="/">Home</a></li>
+    <?php
+    $crumbs = explode("/", $_SERVER["REQUEST_URI"]);
+
+    foreach ($crumbs as $crumb) {
+      if ($crumb != "") {
+        echo "<li class='breadcrumb-item'><a href='#'>" . ucfirst(str_replace(array(".php", "_"), array("", " "), $crumb) . ' ') . "</a></li>";
+      }
+    }
+    ?>
+  </ol>
+</div>
