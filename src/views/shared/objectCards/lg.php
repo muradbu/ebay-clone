a0a5b78fc198c63a11904fa7cf4c80d3328ae783
murad_buyukasik <murad_buyukasik@hotmail.com>
2019-11-26 14:32:29 +0000

    Merged in Masonry (pull request #20)
    Masonry
    
    * Medium en Large size card components toegevoegd
    
    * Card grid component toegevoegd
    
    * Styling aangepast
    
    * Mapstructuur aangepast, grid component verwijderd
    
    * Grid, lg en md card components deleted
    
    Approved-by: Timo Vaassen <timovaassen1@kpnmail.nl>
    Approved-by: TheProudWolf <theproudwolf@hotmail.com>
    Approved-by: murad_buyukasik <murad_buyukasik@hotmail.com>
    Approved-by: Bram Klapwijk <bram.klapwijk00@gmail.com>

---
+++ src/views/shared/objectCards/lg.php
@@ -0,0 +1,18 @@
+<div class="card">
+    <img src="http://via.placeholder.com/350x460" class="card-img-top" alt="#" width="350px" height="469px" />
+    <div class="card-body">
+        <h5 class="card-title">Artikel naam</h5>
+        <div class="row text-center">
+            <div class="col-sm-6 col-md-6 col-lg-6">
+                <p>€ Prijs</p>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-6">
+                <p>Tijd</p>
+            </div>
+        </div>
+        <div class="d-flex justify-content-center">
+            <a href="#" class="btn btn-primary">Verhoog bod</a>
+        </div>
+        
+    </div>
+</div>
