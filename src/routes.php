c57853a4ea9d65fc4fbcbc95adfb84691e53983f
gadget228 <bastiaan228@live.nl>
2020-01-10 14:04:53 +0100

    Veiling toevoegen in navbar

--- src/routes.php
+++ src/routes.php
@@ -38,7 +38,7 @@ switch ($request_uri) {
     case '/contact/:id':
         require isAuthenticated('views/contact.php');
         break;
-    case '/toevoegenproduct':
+    case '/toevoegenveiling':
         require 'views/product/productForm.php';
         break;
     case '/rubrieken/:id':
