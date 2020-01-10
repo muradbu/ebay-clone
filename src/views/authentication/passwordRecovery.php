07e4a6e04fb64a79b5e0b0bb5fe6d4c8ca429914
TheProudWolf <theproudwolf@hotmail.com>
2020-01-10 12:43:41 +0000

    Merged in Wachtwoord-Vergeten (pull request #67)
    Wachtwoord Vergeten
    
    * - temp save
    
    * - Password recovery works
    
    * - Removed comments
    
    * - temp save
    
    * - temp save
    
    * no message
    
    * - Save
    
    * - save
    
    * - save
    
    * - Use the email validator
    
    * - Changed QuestionController get
        - UserController put function working
        - ResetUserPasswordValidator working
        - Passwordrecovery pull request fixes
    
    * - Hash added to password
    
    * - Removed same text
        - Removed unused code
    
    Approved-by: Bram Klapwijk <bram.klapwijk00@outlook.com>
    Approved-by: Imre Boersma <imre.boersma@gmail.com>
    Approved-by: Murad Buyukasik <murad_buyukasik@hotmail.com>

---
+++ src/views/authentication/passwordRecovery.php
@@ -0,0 +1,76 @@
+<?php
+$step = 1;
+require_once('controllers/UserController.php');
+require_once('controllers/QuestionController.php');
+require_once('validators/ResetUserPasswordValidator.php');
+require_once('validators/EmailValidator.php');
+
+if(isset($_POST["submit"])){
+    $errors = EmailValidator::validate($_POST["email"]);
+    if(count($errors) == 1 && $errors["email"] == "Het opgegeven email bestaat al."){
+        $user = UserController::getByEmail($_POST["email"]);
+        $question = QuestionController::get($user["QuestionId"])["Question"];
+        $step = 2;
+    }
+
+    if($step == 2){
+        if(isset($_POST["newpassword"]) && isset($_POST["repeatnewpassword"]) && isset($_POST["answer"])){
+            $errors = ResetUserPasswordValidator::validate($_POST);
+            if(!$errors){
+                UserController::put($user["Username"],["Password" => $_POST["newpassword"]]);
+                echo "<div class='alert alert-success' role='alert'>Wachtwoord aangepast</div>";
+                echo "<meta http-equiv='refresh' content='5; url=/inloggen'>";
+                return;
+            }
+        }
+    }
+}
+?>
+<div class="row justify-content-center">
+    <div class="col-8">
+        <div class="card text-center">
+            <div class="card-body">
+                <h1 class="card-title">Wachtwoord vergeten?</h1>
+                <?php if($step == 1){?>
+                <p class="card-text">Vul hieronder uw email in.</p>
+                <form method="post">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-12">
+                            <input type="email" name="email" class="form-control" placeholder="Emailadres" required>
+                        </div>
+                        <div class="col-lg-12 mt-4">
+                            <button type="submit" name="submit" class="btn-primary btn text-white">Volgende stap</button>
+                        </div>
+                    </div>
+                </form>
+                <?php }?>
+                <?php if($step == 2){?>
+                <p class="card-text">Vul hieronder het antwoord op de geheime vraag in en uw nieuwe wachtwoord.</p>
+                <form method="post">
+                    <input type="hidden" name="email" class="form-control" value="<?php echo $_POST['email']; ?>">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-12 mt-2">
+                            <label class="float-left" for="answer"><?php echo $question; ?>*</label>
+                            <input type="text" value="<?php echo $_POST['answer'] ?? "" ?>" name="answer" class="form-control <?php echo isset($errors['answer']) ? 'is-invalid' : ''; ?>" required>
+                            <div class="invalid-feedback"><?php echo $errors['answer'] ?? ''; ?></div>
+                        </div>
+                        <div class="col-lg-12 mt-2">
+                            <label class="float-left" for="newpassword">Nieuw wachtwoord*</label>
+                            <input type="password" value="<?php echo $_POST['newpassword'] ?? "" ?>" name="newpassword" class="form-control <?php echo isset($errors['newpassword']) ? 'is-invalid' : ''; ?>" required>
+                            <div class="invalid-feedback"><?php echo $errors['newpassword'] ?? ''; ?></div>
+                        </div>
+                        <div class="col-lg-12 mt-2">
+                            <label class="float-left" for="repeatnewpassword">Herhaal nieuw wachtwoord*</label>
+                            <input type="password" value="<?php echo $_POST['repeatnewpassword'] ?? "" ?>" name="repeatnewpassword" class="form-control <?php echo isset($errors['repeatnewpassword']) ? 'is-invalid' : ''; ?>" required>
+                            <div class="invalid-feedback"><?php echo $errors['repeatnewpassword'] ?? ''; ?></div>
+                        </div>
+                        <div class="col-lg-12 mt-4">
+                            <button type="submit" name="submit" class="btn-primary btn text-white">Wijzig wachtwoord</button>
+                        </div>
+                    </div>
+                </form>
+                <?php }?>
+            </div>
+        </div>
+    </div>
+</div>
