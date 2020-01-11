d62576ca193b864211c3a91424b6829712374e45
MuradBuyukasik <murad_buyukasik@hotmail.com>
2020-01-11 10:55:15 +0100

    Product toevoegen foto's tonen en verwijderen

--- src/js/pictureViewer.js
+++ src/js/pictureViewer.js
@@ -1,16 +1,33 @@
+var count = 0;
+
+
 $(function () {
+    var photosVal = [];
+
     var imagesPreview = function (input, placeToInsertImagePreview) {
+
+
+        console.log(photosVal);
+
         if (input.files) {
             var filesAmount = input.files.length;
 
             for (i = 0; i < filesAmount; i++) {
                 var reader = new FileReader();
 
+                count++;
+                if (count > 5) {
+                    return;
+                }
+
+                let random = Math.floor(Math.random() * 10000);
+
                 reader.onload = function (event) {
-                    $($.parseHTML('<img class="py-2 px-2">')).attr('src', event.target.result).appendTo(placeToInsertImagePreview);
+                    $($.parseHTML('<img class="py-2 px-2" onclick="photoPop(' + random + ');" id="' + random + '" />')).attr('src', event.target.result).appendTo(placeToInsertImagePreview);
                 }
 
                 reader.readAsDataURL(input.files[i]);
+                photosVal.push(input.files[i]);
             }
         }
 
@@ -18,6 +35,10 @@ $(function () {
 
     $('#photo').on('change', function () {
         imagesPreview(this, 'div.gallery');
-        console.log($('div.gallery').children.length);
     });
 });
+
+function photoPop(index) {
+    console.log(index);
+    $('#'+index).remove();
+}
