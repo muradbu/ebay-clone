fbe7c17abda43716cacbf0fe63ce9decf6ea6d59
MuradBuyukasik <murad_buyukasik@hotmail.com>
2020-01-13 15:11:11 +0100

    fix

--- src/Config.php
+++ src/Config.php
@@ -11,11 +11,7 @@ class Config
 
     const DATABASE_DATABASE = "iproject1";
 
-<<<<<<< HEAD
-    const ROOT_FOLDER = "/iproject/src";
-=======
     const ROOT_FOLDER = "/src";
->>>>>>> 637ed4f625a171a0219aef32e0394a9a48bf9427
 
     const LIVE_URL = "http://iproject1.icasites.nl/";
 }
